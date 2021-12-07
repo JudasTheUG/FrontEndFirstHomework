aa9f17287d9c917a5483b0be0c21cfe0af608415
Uğur Geleç <37243421+JudasTheUG@users.noreply.github.com>
2021-12-07 17:15:22 +0300

    Created a palindrome project in memory of Barney Stinson

--- src/com/ugurgelec/example/Main.java
+++ src/com/ugurgelec/example/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[])
     {
-        //TO DO
+        String originalValue, reverseValue = "";
+        Scanner enteredValue = new Scanner(System.in);
+        System.out.println("Enter a word or sentence to check if it is a palindrome");
+        originalValue = enteredValue.nextLine();
+        for ( int i = originalValue.length() - 1; i >= 0; i-- )
+            reverseValue = reverseValue + originalValue.charAt(i);
+        if (originalValue.equals(reverseValue))
+            System.out.println("Word or sentence you entered is ... \n wait for it ... \n a PALINDROME");
+        else
+            System.out.println("Word or sentence you entered is ... \n wait for it ... \n NOT a PALINDROME");
     }
 }
