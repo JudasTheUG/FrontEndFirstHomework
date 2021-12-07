497a7bd99495372ca3dcd7af434ca40cb98b2e76
Uğur Geleç <37243421+JudasTheUG@users.noreply.github.com>
2021-12-07 18:17:30 +0300

    Finalized the palindrome project in memory of Barney Stinson

--- src/com/ugurgelec/example/Main.java
+++ src/com/ugurgelec/example/Main.java
@@ -1,19 +1,38 @@
 package com.ugurgelec.example;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        String originalValue, reverseValue = "";
+        Locale trlocale= Locale.forLanguageTag("tr-TR");
+
+        String originalValue , reverseValue ;
+        boolean continueOrNot= true;
         Scanner enteredValue = new Scanner(System.in);
-        System.out.println("Enter a word or sentence to check if it is a palindrome");
-        originalValue = enteredValue.nextLine();
-        for ( int i = originalValue.length() - 1; i >= 0; i-- )
-            reverseValue = reverseValue + originalValue.charAt(i);
-        if (originalValue.equals(reverseValue))
-            System.out.println("Word or sentence you entered is ... \n wait for it ... \n a PALINDROME");
-        else
-            System.out.println("Word or sentence you entered is ... \n wait for it ... \n NOT a PALINDROME");
+        System.out.println("Here is some PALINDROME examples \n Al kazık çak karaya kayarak kaç kızakla \n Do geese see God \n kütük \n bob");
+        while (continueOrNot)
+        {
+                reverseValue = "";
+
+                System.out.println("Enter a word or sentence to check if it is a palindrome");
+                originalValue = enteredValue.nextLine().toUpperCase(trlocale);
+
+                for (int i = originalValue.length() - 1; i >= 0; i--) {
+                    reverseValue += originalValue.charAt(i);
+                }
+                originalValue = originalValue.replace(" ", "").trim();
+                reverseValue = reverseValue.replace(" ", "").trim();
+                if (originalValue.equals(reverseValue)) {
+                    System.out.println("Word or sentence you entered is ... \n ... wait for it ... \n  a PALINDROME!!!");
+                } else {
+                    System.out.println("Word or sentence you entered is ... \n ... wait for it ... \n NOT a PALINDROME!!!");
+                }
+
+                System.out.println("Do you want to continue?\n 0 for NO \n 1 for Yes");
+                continueOrNot= !enteredValue.nextLine().equals("0");
+
+        }
     }
 }
